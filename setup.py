8b1817c0f773cc6bf0c1a1f2937b5439f1a54fcf
Nikolas Tay <niktay94@gmail.com>
2018-10-21 14:01:48 +0800

    Set up unit testing

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+
+setup(
+    name='demoapp',
+    version='0.1.0',
+    author='niktay',
+    description='',
+    install_requires=[
+        'Click==7.0',
+        'Flask==1.0.2',
+        'ItsDangerous==1.0.0',
+        'Jinja2==2.10',
+        'MarkupSafe==1.0',
+        'PyYAML==3.13',
+        'Werkzeug==0.14.1',
+        'aspy.yaml==1.1.1',
+        'cached-property==1.5.1',
+        'cfgv==1.1.0',
+        'identify==1.1.7',
+        'nodeenv==1.3.2',
+        'pre-commit==1.11.2',
+        'pytest==3.9.1',
+        'six==1.11.0',
+        'toml==0.10.0',
+        'virtualenv==16.0.0',
+    ],
+)
